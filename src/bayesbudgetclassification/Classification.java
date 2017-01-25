6bf7e4337987b10363a6d9032e3f393637574e01
Daniel Binggeli <binggeli@puzzle.ch>
2017-01-25 15:10:36 +0100

    implementet the full learning cycle

--- src/bayesbudgetclassification/Classification.java
+++ src/bayesbudgetclassification/Classification.java
@@ -1,27 +1,38 @@
 package bayesbudgetclassification;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 
-import weka.classifiers.Evaluation;
 import weka.classifiers.bayes.NaiveBayes;
 import weka.core.Attribute;
 import weka.core.DenseInstance;
+import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 
 public class Classification {
 
+	/*
+	 * In the main class we create a bayes classifier and calculate the
+	 * probability for all budget positions
+	 * 
+	 */
 	public static void main(String[] args) {
 
 		// get feature vector definition
 		Instances featureVector = createFeatureVector();
-		Instances testData = defineTestdata(featureVector);
+		// get the trainingsdata
 		Instances trainingsSet = buildTrainingsSet();
+		// get the data we like to proof
+		Instances testData = defineTestdata(featureVector);
+		testData.setClassIndex(trainingsSet.numAttributes() - 1);
 
 		try {
-
-			testData.setClassIndex(trainingsSet.numAttributes() - 1);
 			/*
 			 * Create the classifier from weka
 			 */
@@ -34,11 +45,23 @@ public class Classification {
 			 * get the probability for each class
 			 */
 			double[] probability = naivebayes.distributionForInstance(testData.firstInstance());
-			
-			
-			System.out.println(Arrays.toString(probability));
 
-			
+			/*
+			 * now we store all the probabilities in a hashmap togehter with the
+			 * relatet budgetpositions
+			 */
+			HashMap<Double, String> budgetmap = new LinkedHashMap<Double, String>();
+			ArrayList<String> budgetPositions = getBudgetPositions();
+			for (int i = 0; i < budgetPositions.size(); i++) {
+				budgetmap.put(probability[i], budgetPositions.get(i));
+			}
+			// we try to get the highest possibility of all budget-positions
+			double maxValueInMap = Collections.max(budgetmap.keySet());
+
+			int budgetpositionindex = budgetPositions.indexOf(budgetmap.get(maxValueInMap));
+
+			// finaly we add the testet data and the budegetposition back to our trainingsset
+			learnNewTrainingData(budgetpositionindex);
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -46,7 +69,7 @@ public class Classification {
 	}
 
 	/*
-	 * 
+	 * In this class we create a
 	 */
 	public static Instances buildTrainingsSet() {
 
@@ -66,20 +89,46 @@ public class Classification {
 		return null;
 	}
 
+	public static void learnNewTrainingData(int learnedclass) throws Exception {
+		/*
+		 * Load the trainingset add the new trained data to the set and save the
+		 * file.
+		 */
+		Instances testdata = defineTestdata(createFeatureVector());
+		Instances trainingsSet = buildTrainingsSet();
+		Instance newdataset = testdata.get(0);
+
+		double[] instanceValue1 = newdataset.toDoubleArray();
+		double[] instanceValue = Arrays.copyOf(instanceValue1, instanceValue1.length + 1);
+		instanceValue[3] = learnedclass;
+
+		try {
+
+			trainingsSet.add(new DenseInstance(1.0, instanceValue));
+
+			BufferedWriter writer = new BufferedWriter(new FileWriter("learned_budget.arff"));
+			writer.write(trainingsSet.toString());
+			writer.flush();
+			writer.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
 	/*
 	 * 
 	 */
 	public static Instances defineTestdata(Instances testData) {
 
 		// set testset
-		double[] instanceValue1 = new double[testData.numAttributes()-1];
+		double[] instanceValue1 = new double[testData.numAttributes() - 1];
 
 		instanceValue1[0] = 1;
 		instanceValue1[1] = 0;
 		instanceValue1[2] = 1;
 		testData.add(new DenseInstance(1.0, instanceValue1));
-
-		System.out.println(testData);
 
 		return testData;
 	}
@@ -124,6 +173,16 @@ public class Classification {
 		amount.add("riesig");
 		attribute.add(new Attribute("Betrag", amount));
 
+		ArrayList<String> classVal = getBudgetPositions();
+		attribute.add(new Attribute("Klasse", classVal));
+
+		Instances featureVectorDefinition = new Instances("TestInstance", attribute, 0);
+
+		return featureVectorDefinition;
+	}
+
+	public static ArrayList<String> getBudgetPositions() {
+
 		ArrayList<String> classVal = new ArrayList<String>();
 		classVal.add("Nahrungsmittel");
 		classVal.add("Persönliche Ausgaben");
@@ -136,11 +195,8 @@ public class Classification {
 		classVal.add("Miete");
 		classVal.add("Steuern");
 		classVal.add("Krankenkasse");
-		attribute.add(new Attribute("Klasse", classVal));
 
-		Instances featureVectorDefinition = new Instances("TestInstance", attribute, 0);
-
-		return featureVectorDefinition;
+		return classVal;
 	}
 
 }
